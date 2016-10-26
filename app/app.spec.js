db4ab9360cfa9ff8df49fc9727f6180c43380c5b
harshanerella <nerellapriyank@gmail.com>
2016-10-26 14:32:06 +0530

    step-2 Angular Templates

---
+++ app/app.spec.js
@@ -0,0 +1,12 @@
+describe('PhoneListController', function() {
+	beforeEach(module('phonecatApp'));
+	it('should create phones model with 3 phones', inject(function($controller){
+          
+            var scope={};
+            var ctrl=$controller('PhoneListController',{$scope:scope});
+            expect(scope.phones.length).toBe(3);
+            expect(scope.name).toBe('world');
+
+
+	}));
+});
